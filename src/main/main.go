3a804e93d612c707f7ff46593f26142168ce751d
王彬彬 <769558579@qq.com>
2019-01-03 15:16:11 +0800

    初始化

---
+++ src/main/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/Jarvens/quotation/protocol"
+
+func main() {
+
+}
--- src/protocol/protocol.go
+++ src/protocol/protocol.go
@@ -1,4 +1,12 @@
 package protocol
 
 type TcpProtocol struct {
+	Code        byte
+	Version     byte
+	Header      string
+	RequestType byte
+	ClientType  byte
+	Content     string
+	HeaderLen   int16
+	ContentLen  int16
 }
